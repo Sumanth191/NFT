ac834dbb19749f3e7903a5bc09509485a608931e
Sumanth <sumanthreddy573@gmail.com>
2023-10-30 01:27:48 -0700

    Update commit

--- Framework_Inputs/Bat_File/1/petclinictestplan_10Volume.bat
+++ Framework_Inputs/Bat_File/1/petclinictestplan_10Volume.bat
@@ -1,4 +1,4 @@
 @ECHO OFF 
-set PropertyFilePath=C:\Users\nftuser\Documents\FrameworkRepo\Framework_Inputs\Property_File\petclinictestplan_Nil.properties
-cd Bin
+set PropertyFilePath=C:\Users\nftuser\Documents\FrameworkRepo\Framework_Inputs\Property_File\petclinictestplan_10Volume.properties
+cd C:\Users\nftuser\Documents\apache-jmeter-5.5\bin
 jmeter -q %PropertyFilePath% -n -t C:\Users\nftuser\Documents\FrameworkRepo\Framework_Inputs\Scripts\petclinictestplan.jmx -l C:\Users\nftuser\Documents\FrameworkRepo\Framework_Outputs\Logs\Jmeter\JTL_Log\JTL_petclinictestplan.jtl
